eddffe42ce7f64150122e40c03d5aa514387f81f
Jack Dickson <jackowendickson@gmail.com>
2022-05-16 19:49:45 +0100

    Started Extensions

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,24 @@
+import attractions.Attraction;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractionsList;
+    private ArrayList<Stall> stallList;
+
+
+    public ThemePark(ArrayList<Attraction> attractionsList, ArrayList<Stall> stallList) {
+        this.attractionsList = attractionsList;
+        this.stallList = stallList;
+    }
+
+    public ArrayList<Attraction> getAttractionsList() {
+        return attractionsList;
+    }
+
+    public ArrayList<Stall> getStallList() {
+        return stallList;
+    }
 }
